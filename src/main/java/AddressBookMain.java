20f1fdfb6040263cab068d26de16b1001cb35dcc
Prajwal Sanjay Thorat <prajwalthorat9007@gmail.com>
2026-03-03 12:29:22 +0530

    [Prajwal]:UC1:Created Contact class and initialized in Main

--- src/main/java/AddressBookMain.java
+++ src/main/java/AddressBookMain.java
@@ -1,5 +1,8 @@
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("Welcome to Address Book Program");
+        Contact contact = new Contact("John", "Doe", "123 Main St", "Cityville", "State", "12345", "1234567890",
+                "john@example.com");
+        System.out.println("Created test contact: " + contact);
     }
 }
